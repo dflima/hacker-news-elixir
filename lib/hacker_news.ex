e6f1720a9fab39bb7ef95bd0c6cf06ccb48a829d
Danilo <danilo.lima@sumup.com>
2021-07-04 21:05:36 -0300

    first commit
    - app generated with `mix new hacker_news --sup`

---
+++ lib/hacker_news.ex
@@ -0,0 +1,18 @@
+defmodule HackerNews do
+  @moduledoc """
+  Documentation for HackerNews.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HackerNews.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
