22e8776d6c4db7ab65b5e665768d5783e4f7970f
Danilo <danilo.lima@sumup.com>
2021-07-09 15:07:52 -0300

    removed boilerplate code

--- lib/hacker_news.ex
+++ lib/hacker_news.ex
@@ -1,18 +1,2 @@
 defmodule HackerNews do
-  @moduledoc """
-  Documentation for HackerNews.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HackerNews.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/hacker_news_test.exs
+++ test/hacker_news_test.exs
@@ -1,8 +1,3 @@
 defmodule HackerNewsTest do
   use ExUnit.Case
-  doctest HackerNews
-
-  test "greets the world" do
-    assert HackerNews.hello() == :world
-  end
 end
