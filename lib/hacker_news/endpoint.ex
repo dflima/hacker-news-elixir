c8607aba83da05ba9066f9a8649a88b52d0e01ac
Danilo <danilo.lima@sumup.com>
2021-07-09 13:59:01 -0300

    added pagination

--- lib/hacker_news/endpoint.ex
+++ lib/hacker_news/endpoint.ex
@@ -1,13 +1,31 @@
 defmodule HackerNews.Endpoint do
   use Plug.Router
 
+  plug(Plug.Static,
+    at: "/",
+    from: :hacker_news
+  )
+
   plug(Plug.Logger)
   plug(:match)
-  plug(Plug.Parsers, parsers: [:json], json_decoder: Jason)
+
+  plug(Plug.Parsers,
+    parsers: [:json],
+    pass: ["application/json"],
+    json_decoder: Jason
+  )
+
   plug(:dispatch)
 
   get "/stories" do
-    response = HackerNews.Storage.get_all() |> Jason.encode!()
+    page = parse_page_param(conn.params)
+
+    response =
+      HackerNews.Storage.get_all()
+      |> Enum.chunk_every(10)
+      |> Enum.at(page)
+      |> Jason.encode!()
+
     send_resp(conn, 200, response)
   end
 
@@ -20,4 +38,10 @@ defmodule HackerNews.Endpoint do
   match _ do
     send_resp(conn, 404, "Nothing to see here.")
   end
+
+  defp parse_page_param(%{"page" => page}) do
+    String.to_integer(page)
+  end
+
+  defp parse_page_param(_), do: 1
 end
